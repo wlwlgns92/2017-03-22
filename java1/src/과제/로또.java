5c90d53d626fa1ffbfe8538df32f335b7886863e
wlwlgns92 <wlwlgns92@gmail.com>
2022-01-05 21:38:43 +0900

    asd

---
+++ java1/src/과제/로또.java
@@ -0,0 +1,24 @@
+package 과제;
+
+import java.util.HashSet;
+
+public class 로또 {
+
+	public static void main(String[] args) {
+
+		HashSet<String> set = new HashSet<>();
+
+		set.add("java");
+		set.add("mysql");
+		set.add("JSP");
+		set.add("java");
+		set.add("Spring");
+
+		
+		System.out.println(set);
+		System.out.println(set.size());
+		
+		
+	}
+
+}
